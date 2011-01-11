641c8b7db57c1b65f4f43de01d6d236e64617f54
Nishant <nick722@gmail.com>
2011-01-11 15:47:38 -0500

    Created a new Engine class to manage all objects created in space such as a LineSeg. This class also returns a Graphics2D Shape object that will be printed on screen using vector projections.

--- src/Engine.java
+++ src/Engine.java
@@ -1,17 +1,57 @@
+import java.awt.Graphics;
+import java.awt.Shape;
+import java.awt.geom.Line2D;
+import java.util.ArrayList;
+
 
 public class Engine {
 
-	public void draw(Object o) {
-		if(o instanceof LineSeg) {
-			LineSeg ls = (LineSeg) o;
-			drawLineSeg(ls);
-		}
-		else {
-			System.out.println("Not a valid vector.");
+	private static ArrayList<LineSeg> lineSegs;
+	
+	public Engine() {
+		lineSegs = new ArrayList<LineSeg>();
+	}
+	
+	public void addLineSeg(LineSeg ls) {
+		if(!lineSegs.contains(ls)) {
+			lineSegs.add(ls);
 		}
 	}
 	
-	public void drawLineSeg( LineSeg ls ) {
-		System.out.println("DRAWING LINESEG NOW");
+	public void removeLineSeg(LineSeg ls) {
+		if(!lineSegs.contains(ls)) {
+			lineSegs.remove(ls);
+		}
 	}
+	
+	public ArrayList<LineSeg> getLineSegs() {
+		return lineSegs;
+	}
+	
+	
+	public Shape getShape(Object o) {
+		if(o instanceof LineSeg) {
+			LineSeg ls = (LineSeg) o;
+			if(!lineSegs.contains(ls)) {
+				lineSegs.add(ls);
+			}
+			return getShapeOfLineSeg(ls);
+		}
+		else {
+			System.out.println("Not a valid vector.");
+			return null;
+		}
+	}
+	
+	public Shape getShapeOfLineSeg( LineSeg ls ) {
+		//Must change this:
+		int x1 = ls.x1;
+		int y1 = ls.y1;
+		int x2 = ls.x2;
+		int y2 = ls.y2;
+
+		Shape line = new Line2D.Double(x1, y1, x2, y2);
+		return line;
+	}
+	
 }
---
+++ src/MainApplet.java
@@ -0,0 +1,103 @@
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.geom.Line2D;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+
+public class MainApplet extends Applet implements KeyListener, Runnable {
+	
+	boolean wKey, aKey, sKey, dKey = false;
+	Camera c = new Camera(2,2,2);
+	Engine e = new Engine();
+	
+	public void init() {
+		ExecutorService exec = Executors.newCachedThreadPool();
+		exec.execute(this);
+		
+		setSize(640, 480);
+		addKeyListener(this);
+		
+		e.addLineSeg(new LineSeg(0,0,0, 100,200,10));
+	}
+	
+	public void paint(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		for(LineSeg ls : e.getLineSegs()) {
+			g2.draw(e.getShape(ls));
+		}
+	}
+
+	
+	
+	public void keyPressed(KeyEvent e) {
+		//System.out.println(e.getKeyCode());
+		if(e.getKeyCode()==87) { //w
+			wKey=true;
+		}
+		if(e.getKeyCode()==65) { //a
+			aKey=true;
+		}
+		if(e.getKeyCode()==83) { //s
+			sKey=true;
+		}
+		if(e.getKeyCode()==68) { //d
+			dKey=true;
+		}
+		
+		repaint();
+	}
+
+	public void keyReleased(KeyEvent e) {
+		if(e.getKeyCode()==87) { //w
+			wKey=false;
+		}
+		if(e.getKeyCode()==65) { //a
+			aKey=false;
+		}
+		if(e.getKeyCode()==83) { //s
+			sKey=false;
+		}
+		if(e.getKeyCode()==68) { //d
+			dKey=false;
+		}
+		repaint();
+	}
+
+	public void keyTyped(KeyEvent e) {
+		
+	}
+
+
+	public void run() {
+		while(true) {
+			try {
+				if(wKey) {
+					System.out.println("w");
+					c.moveIn();
+				}
+				if(aKey) {
+					System.out.println("a");
+					c.moveOut();
+				}
+				if(sKey) {
+					System.out.println("s");
+					c.panLeft();
+				}
+				if(dKey) {
+					System.out.println("d");
+					c.panRight();
+				}
+				
+				Thread.sleep(100);
+				//System.out.println("sleep");
+			} catch (InterruptedException e) {}
+		}
+	}
+	
+	
+	
+}
